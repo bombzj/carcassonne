83c7086905b6c9f144ad29da31cee577be3b0cf1
bombzj <bombzj@gmail.com>
2020-11-14 09:28:52 +0800

    update farm connection data

--- data.js
+++ data.js
@@ -10,7 +10,7 @@ const allColors = [
 
 const tileTypes = [
 	{	// (0)	1 roads	
-		count:	4,
+		count:	8,
 		connect : [farm, road, farm, road],	// top left bottom right
 		place: [
 			[0.5, 0.25, farm, [0, 1, 2, 7]],
@@ -45,7 +45,7 @@ const tileTypes = [
 		]
 	},
 	{	// (4)	road+city
-		count:	4,
+		count:	2,
 		connect : [farm, road, city, road],
 		place: [
 			[0.5, 0.2, farm, [0, 1, 2, 7]],
@@ -55,7 +55,7 @@ const tileTypes = [
 		]
 	},
 	{	// (5)	turn road
-		count:	4,
+		count:	9,
 		connect : [farm, farm, road, road],
 		place: [
 			[0.3, 0.3, farm, [0,1,2,3,4,7]],
@@ -67,27 +67,27 @@ const tileTypes = [
 		count:	1,
 		connect : [river, city, river, road],
 		place: [
-			[0.28, 0.15, farm, []],
-			[0.85, 0.2, farm, []],
+			[0.28, 0.15, farm, [1]],
+			[0.85, 0.2, farm, [0,7]],
 			[0.15, 0.5, city, [1]],
 			[0.5, 0.5, road, [3]],
-			[0.3, 0.8, farm, []],
-			[0.8, 0.8, farm, []],
+			[0.3, 0.8, farm, [4]],
+			[0.8, 0.8, farm, [5,6]],
 		]
 	},
 	{	// (7)	lake		
 		count:	1,
 		connect : [river, road, river, road],
 		place: [
-			[0.2, 0.2, farm, []],
-			[0.8, 0.2, farm, []],
+			[0.2, 0.2, farm, [1,2]],
+			[0.8, 0.2, farm, [0,7]],
 			[0.5, 0.5, road, [1, 3]],
-			[0.25, 0.75, farm, []],
-			[0.8, 0.75, farm, []],
+			[0.25, 0.75, farm, [2,3]],
+			[0.8, 0.75, farm, [4,5]],
 		]
 	},
 	{	// (8)			
-		count:	4,
+		count:	1,
 		connect : [city, city, road, city],
 		place: [
 			[0.5, 0.3, city, [0, 1, 3]],
@@ -97,22 +97,22 @@ const tileTypes = [
 		]
 	},
 	{	// (9)			
-		count:	4,
+		count:	2,
 		connect : [city, farm, road, road],
 		place: [
 			[0.5, 0.2, city, [0]],
-			[0.25, 0.7, farm, []],
+			[0.25, 0.7, farm, [2,3,4,7]],
 			[0.58, 0.58, road, [2, 3]],
-			[0.8, 0.8, farm, []],
+			[0.8, 0.8, farm, [5,6]],
 		]
 	},
 	{	// (10)			
-		count:	4,
+		count:	2,
 		connect : [road, city, city, road],
 		place: [
-			[0.85, 0.15, farm, []],
+			[0.85, 0.15, farm, [0,7]],
 			[0.7, 0.3, road, [0, 3]],
-			[0.5, 0.5, farm, []],
+			[0.5, 0.5, farm, [1,6]],
 			[0.2, 0.8, city, [1, 2]],
 		]
 	},
@@ -125,18 +125,18 @@ const tileTypes = [
 		]
 	},
 	{	// (12)			
-		count:	4,
+		count:	2,
 		connect : [city, city, road, road],
 		star : true,
 		place: [
 			[0.2, 0.2, city, [0, 1]],
-			[0.5, 0.5, farm, []],
+			[0.5, 0.5, farm, [4,7]],
 			[0.7, 0.7, road, [2, 3]],
-			[0.85, 0.85, farm, []],
+			[0.85, 0.85, farm, [5,6]],
 		]
 	},
 	{	// (13)			
-		count:	4,
+		count:	1,
 		connect : [farm, city, farm, city],
 		star : true,
 		place: [
@@ -173,9 +173,9 @@ const tileTypes = [
 		count:	4,
 		connect : [road, road, city, road],
 		place: [
-			[0.15, 0.15, farm, []],
-			[0.85, 0.15, farm, []],
-			[0.5, 0.53, farm, []],
+			[0.15, 0.15, farm, [1,2]],
+			[0.85, 0.15, farm, [0,7]],
+			[0.5, 0.53, farm, [3,6]],
 			[0.5, 0.15, road, [0]],
 			[0.2, 0.4, road, [1]],
 			[0.8, 0.43, road, [3]],
@@ -192,7 +192,7 @@ const tileTypes = [
 		]
 	},
 	{	// (19)			
-		count:	4,
+		count:	3,
 		connect : [city, city, farm, city],
 		place: [
 			[0.5, 0.3, city, [0, 1, 3]],
@@ -217,58 +217,58 @@ const tileTypes = [
 		]
 	},
 	{	// (22)			
-		count:	4,
+		count:	2,
 		connect : [city, city, road, city],
 		star : true,
 		place: [
 			[0.5, 0.3, city, [0, 1, 3]],
-			[0.25, 0.85, farm, []],
+			[0.25, 0.85, farm, [4]],
 			[0.5, 0.8, road, [2]],
-			[0.75, 0.85, farm, []],
+			[0.75, 0.85, farm, [5]],
 		]
 	},
 	{	// (23)			
-		count:	4,
+		count:	1,
 		connect : [farm, city, farm, city],
 		place: [
-			[0.5, 0.05, farm, []],
+			[0.5, 0.05, farm, [0,1]],
 			[0.5, 0.4, city, [1, 3]],
-			[0.5, 0.87, farm, []],
+			[0.5, 0.87, farm, [4,5]],
 		]
 	},
 	{	// (24)			
-		count:	4,
+		count:	2,
 		connect : [road, farm, city, road],
 		place: [
-			[0.85, 0.15, farm, []],
+			[0.85, 0.15, farm, [0,7]],
 			[0.65, 0.35, road, [0, 3]],
-			[0.3, 0.5, farm, []],
+			[0.3, 0.5, farm, [1,2,3,6]],
 			[0.5, 0.87, city, [2]],
 		]
 	},
 	{	// (25)			
-		count:	4,
+		count:	1,
 		connect : [city, city, farm, city],
 		star : true,
 		place: [
 			[0.5, 0.3, city, [0, 1, 3]],
-			[0.5, 0.85, farm, []],
+			[0.5, 0.85, farm, [4,5]],
 		]
 	},
 	{	// (26)			
 		count:	4,
 		connect : [farm, road, road, road],
 		place: [
-			[0.5, 0.2, farm, []],
-			[0.2, 0.8, farm, []],
-			[0.8, 0.8, farm, []],
+			[0.5, 0.2, farm, [0,1,2,7]],
+			[0.2, 0.8, farm, [2,3]],
+			[0.8, 0.8, farm, [4,5]],
 			[0.15, 0.5, road, [1]],
 			[0.85, 0.5, road, [3]],
 			[0.5, 0.8, road, [2]],
 		]
 	},
 	{	// (27)			
-		count:	4,
+		count:	2,
 		connect : [farm, farm, road, farm],
 		place: [
 			[0.8, 0.2, farm, [1,2,3,4,5,6,7,0]],
@@ -288,16 +288,16 @@ const tileTypes = [
 		count:	1,
 		connect : [river, farm, river, farm],
 		place: [
-			[0.15, 0.5, farm, []],
-			[0.85, 0.5, farm, []],
+			[0.15, 0.5, farm, [1,2,3,4]],
+			[0.85, 0.5, farm, [0,5,6,7]],
 		]
 	},
 	{	// (30)	lake		
 		count:	1,
 		connect : [river, river, city, city],
 		place: [
-			[0.1, 0.1, farm, []],
-			[0.5, 0.5, farm, []],
+			[0.1, 0.1, farm, [1,2]],
+			[0.5, 0.5, farm, [0,3]],
 			[0.85, 0.85, city, [2, 3]],
 		]
 	},
@@ -305,9 +305,9 @@ const tileTypes = [
 		count:	1,
 		connect : [farm, river, road, river],
 		place: [
-			[0.15, 0.15, farm, []],
-			[0.1, 0.9, farm, []],
-			[0.9, 0.9, farm, []],
+			[0.15, 0.15, farm, [7,0,1,2]],
+			[0.1, 0.9, farm, [3,4]],
+			[0.9, 0.9, farm, [5,6]],
 			[0.5, 0.55, road, [2]],
 		]
 	},
@@ -315,26 +315,26 @@ const tileTypes = [
 		count:	1,
 		connect : [road, river, river, road],
 		place: [
-			[0.15, 0.85, farm, []],
-			[0.3, 0.2, farm, []],
+			[0.15, 0.85, farm, [3,4]],
+			[0.3, 0.2, farm, [1,2,5,6]],
 			[0.7, 0.3, road, [0, 3]],
-			[0.85, 0.15, farm, []],
+			[0.85, 0.15, farm, [0,7]],
 		]
 	},
 	{	// (33)	lake		
 		count:	1,
 		connect : [river, farm, river, farm],
 		place: [
-			[0.15, 0.5, farm, []],
-			[0.85, 0.2, farm, []],
+			[0.15, 0.5, farm, [1,2,3,4]],
+			[0.85, 0.2, farm, [0,5,6,7]],
 		]
 	},
 	{	// (34)	lake		
 		count:	1,
 		connect : [river, farm, farm, river],
 		place: [
-			[0.3, 0.6, farm, []],
-			[0.9, 0.1, farm, []],
+			[0.3, 0.6, farm, [1,2,3,4,5,6]],
+			[0.9, 0.1, farm, [0,7]],
 		]
 	},
 ]
